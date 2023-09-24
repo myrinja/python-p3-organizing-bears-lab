a4b2e28cb5503c42006916404837142566bbdf73
myrinja <myrinjau98@gmail.com>
2023-09-25 00:17:01 +0300

    update all

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO  bears(name,sex) VALUES("Mr. Chocolate","M");
+INSERT INTO  bears(name,sex) VALUES("Rowdy","M");
+INSERT INTO  bears(name,sex) VALUES("Tabitha","F");
+INSERT INTO  bears(name,sex) VALUES("Sergeant Brown","M");
+INSERT INTO  bears(name,sex) VALUES("Melissa","F");
+INSERT INTO  bears(name,sex) VALUES("Grinch","M");
+INSERT INTO  bears(name,sex) VALUES("Wendy","F");
+INSERT INTO  bears(name,sex) VALUES(NULL,NULL);
+
+--or 
+
+--INSERT INTO bears (name, sex)
+--VALUES
+    --('Mr. Chocolate', 'M'),
+    --('Rowdy', 'M'),
+    --('Tabitha', 'F'),
+   -- ('Sergeant Brown', 'M'),
+   -- ('Melissa', 'F'),
+    --('Grinch', 'M'),
+   -- ('Wendy', 'F'),
+    --(NULL, NULL);
+
+
+
